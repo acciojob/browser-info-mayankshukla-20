bb701b36b22ac852d20b65dd2b601345a3e055ed
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2023-12-20 14:46:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const browserName = navigator.appName;
+const version = navigator.appVersion;
+const divElement = document.getElementById("browser-info");
+const para = document.createElement("p");
+para.innerText = "You are using " + browserName + "version " + version;
+divElement.appendChild(para);
